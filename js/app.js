1b24fb3a20fd5aac7cafc5855d9b1a09ce276a06
Ulysses Leal <105395257+ULYBR@users.noreply.github.com>
2022-08-02 11:09:36 -0300

    atualização do cadastro-login.
    editei tanto o login quanto o cadastro.
    essa parte está pronta.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+var btnsSignin = document.querySelector("#signin");
+var btnsSignup = document.querySelector("#signup");
+
+var body = document.querySelector("body");
+
+btnsSignin.addEventListener("click", function () {
+    body.className ="sign-in-js";
+});
+btnsSignup.addEventListener("click", function () {
+    body.className ="sign-up-js";
+});
